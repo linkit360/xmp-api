cfb3176bbd7f29d86c898b5881a943cc470f302a
Helena Nevskaya <helena.nevskaya@ya.ru>
2017-05-26 15:36:48 +0300

    campaign code is campaign code in json

--- src/structs/structs.go
+++ src/structs/structs.go
@@ -35,7 +35,7 @@ type Aggregate struct {
 	ProviderName           string `json:"provider_name,omitempty"`
 	OperatorCode           int64  `json:"operator_code,omitempty"`
 	ReportAt               int64  `json:"report_at,omitempty"`
-	CampaignCode           string `json:"id_campaign,omitempty"`
+	CampaignCode           string `json:"campaign_code,omitempty"`
 	LpHits                 int64  `json:"lp_hits,omitempty"`
 	LpMsisdnHits           int64  `json:"lp_msisdn_hits,omitempty"`
 	MoTotal                int64  `json:"mo,omitempty"`
