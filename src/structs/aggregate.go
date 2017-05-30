c03cd7b33153e726cff1e02807372b330e5b7aa2
Caravus <caravus@caravus.org>
2017-05-30 15:13:39 +0300

    status

--- src/structs/aggregate.go
+++ src/structs/aggregate.go
@@ -10,19 +10,19 @@ type AggregateRequest struct {
 }
 
 type Aggregate struct {
-	Provider               string `json:"provider,omitempty"`
-	OperatorCode           int64  `json:"operator_code,omitempty"`
-	ReportAt               int64  `json:"report_at,omitempty"`
-	CampaignCode           string `json:"campaign_code,omitempty"`
-	LpHits                 int64  `json:"lp_hits,omitempty"`
-	LpMsisdnHits           int64  `json:"lp_msisdn_hits,omitempty"`
-	MoTotal                int64  `json:"mo,omitempty"`
-	MoChargeSuccess        int64  `json:"mo_charge_success,omitempty"`
-	MoChargeSum            int64  `json:"mo_charge_sum,omitempty"`
-	MoChargeFailed         int64  `json:"mo_charge_failed,omitempty"`
-	MoRejected             int64  `json:"mo_rejected,omitempty"`
-	Outflow                int64  `json:"outflow,omitempty"`
-	RenewalTotal           int64  `json:"renewal,omitempty"`
+	Provider        string `json:"provider"`
+	OperatorCode    int64  `json:"operator_code"`
+	ReportAt        int64  `json:"report_at"`
+	CampaignCode    string `json:"campaign_code"`
+	LpHits          int64  `json:"lp_hits,omitempty"`
+	LpMsisdnHits    int64  `json:"lp_msisdn_hits,omitempty"`
+	MoTotal         int64  `json:"mo,omitempty"`
+	MoChargeSuccess int64  `json:"mo_charge_success,omitempty"`
+	MoChargeSum     int64  `json:"mo_charge_sum,omitempty"`
+	MoChargeFailed  int64  `json:"mo_charge_failed,omitempty"`
+	MoRejected      int64  `json:"mo_rejected,omitempty"`
+	Outflow         int64  `json:"outflow,omitempty"`
+	RenewalTotal    int64  `json:"renewal,omitempty"`
 	RenewalChargeSuccess   int64  `json:"renewal_charge_success,omitempty"`
 	RenewalChargeSum       int64  `json:"renewal_charge_sum,omitempty"`
 	RenewalFailed          int64  `json:"renewal_failed,omitempty"`
