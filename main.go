8fa8d7c43ca9362b45d859409927e262d87935e1
Caravus <caravus@caravus.org>
2017-06-07 17:26:13 +0300

    svc code

--- main.go
+++ main.go
@@ -182,7 +182,7 @@ func testInitialization() {
 		},
 	).Info("Request Done")
 
-	//log.Info(resp.Operators[2])
+	//log.Info(resp.Services["c4bc3983-648c-45f9-a5b3-e1f6aa82db90"].Code)
 }
 
 func testUpdateRead() {
