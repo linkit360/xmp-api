21414c195ffb4632798b54fb01ef28fb99bf9e34
Caravus <caravus@caravus.org>
2017-06-08 16:21:31 +0300

    service_id

--- src/structs/campaigns.go
+++ src/structs/campaigns.go
@@ -1,19 +1,19 @@
 package xmp_api_structs
 
 type Campaign struct {
-	Id               string `gorm:"primary_key",json:"id"`        // UUID
-	Code             string `gorm:"column:id_old",json:"code"`    // previous id
-	Title            string `json:"title,omitempty"`              //
-	Link             string `json:"link"`                         //
-	Lp               string `json:"lp"`                           // UUID
-	Hash             string `json:"hash,omitempty"`               //
-	ServiceCode      string `json:"service_code,omitempty"`       // previous service code
-	AutoClickRatio   int64  `json:"auto_click_ratio,omitempty"`   //
-	AutoClickEnabled bool   `json:"auto_click_enabled,omitempty"` //
-	PageSuccess      string `json:"page_success,omitempty"`       //
-	PageError        string `json:"page_error,omitempty"`         //
-	PageThankYou     string `json:"page_thank_you,omitempty"`     //
-	PageWelcome      string `json:"page_welcome,omitempty"`       //
+	Id               string `gorm:"primary_key",json:"id"`               // UUID
+	Code             string `json:"id_old",gorm:"column:id_old"`         // previous id
+	Title            string `json:"title,omitempty"`                     //
+	Link             string `json:"link"`                                //
+	Lp               string `json:"lp"`                                  // UUID
+	Hash             string `json:"hash,omitempty"`                      //
+	ServiceId        string `gorm:"column:id_service",json:"id_service"` // service ID / UUID
+	AutoClickRatio   int64  `json:"auto_click_ratio,omitempty"`          //
+	AutoClickEnabled bool   `json:"auto_click_enabled,omitempty"`        //
+	PageSuccess      string `json:"page_success,omitempty"`              //
+	PageError        string `json:"page_error,omitempty"`                //
+	PageThankYou     string `json:"page_thank_you,omitempty"`            //
+	PageWelcome      string `json:"page_welcome,omitempty"`              //
 }
 
 // Tablenames for GORM
