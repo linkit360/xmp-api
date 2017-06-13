60982b716b996872406b4c3b72a730969134d133
Caravus <caravus@caravus.org>
2017-06-13 16:08:40 +0300

    bl

--- src/server/src/base/getBlacklist.go
+++ src/server/src/base/getBlacklist.go
@@ -1,21 +1,92 @@
 package base
 
 import (
+	"archive/zip"
+	"context"
+	"fmt"
+	"os"
 	"strconv"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/linkit360/xmp-api/src/structs"
 )
 
-func GetBlacklist(id_provider int) ([]string, error) {
-	//var err error
-	out := make([]string, 0)
+func GetBlacklist(id_provider int) (string, error) {
 	data := make([]xmp_api_structs.Blacklist, 0)
+	prep := make([]string, 0)
 
 	// Get campaigns by services ids
-	db.Where("id_provider = ?", id_provider).Find(&data)
+	db.Select("msisdn").Where("id_provider = ?", id_provider).Find(&data)
 	for _, blacklist := range data {
-		out = append(out, strconv.Itoa(blacklist.Msisdn))
+		prep = append(prep, strconv.Itoa(blacklist.Msisdn))
+	}
+	text := strings.Join(prep, "\n")
+
+	// ZIP
+	filename := strconv.Itoa(id_provider) + "_" + time.Now().Format("20060102150405")
+	defer os.Remove(filename)
+
+	zipfile, err := os.Create(filename)
+	if err != nil {
+		log.Error(err)
+	}
+	defer zipfile.Close()
+
+	w := zip.NewWriter(zipfile)
+	f, err := w.Create("blacklist")
+	if err != nil {
+		log.Error(err)
 	}
 
-	return out, nil
+	_, err = f.Write([]byte(text))
+	if err != nil {
+		log.Error(err)
+	}
+
+	err = w.Close()
+	if err != nil {
+		log.Error(err)
+	}
+
+	// AWS S3
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("ap-southeast-1"),
+		Credentials: credentials.NewStaticCredentials(
+			cfgAws.Id,
+			cfgAws.Secret,
+			"",
+		),
+	})
+	if err != nil {
+		log.Error("Cannot init S3 Session ", err)
+	}
+
+	svc := s3.New(sess)
+	ctx := context.Background()
+
+	_, err = svc.PutObjectWithContext(ctx, &s3.PutObjectInput{
+		Bucket: aws.String("xmp-blacklist"),
+		Key:    aws.String(filename),
+		Body:   zipfile,
+	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok && aerr.Code() == request.CanceledErrorCode {
+			// If the SDK can determine the request or retry delay was canceled
+			// by a context the CanceledErrorCode error code will be returned.
+			fmt.Fprintf(os.Stderr, "upload canceled due to timeout, %v\n", err)
+		} else {
+			fmt.Fprintf(os.Stderr, "failed to upload object, %v\n", err)
+		}
+	}
+
+	return filename, nil
 }
--- src/server/src/config/config.go
+++ src/server/src/config/config.go
@@ -16,13 +16,20 @@ type DbConfig struct {
 	Password string `json:"password"`
 }
 
-func LoadConfig() DbConfig {
+type AwsConfig struct {
+	Id     string `json:"key"`
+	Secret string `json:"secret"`
+}
+
+func LoadConfig() (DbConfig, AwsConfig) {
 	log.SetFormatter(new(prefixed.TextFormatter))
 	log.SetLevel(log.DebugLevel)
 	var err error
 
-	var envConfigFile string = "/config/" + EnvString("PROJECT_ENV", "development") + "/db.json"
-	dat, err := ioutil.ReadFile(envConfigFile)
+	var envStr = "/config/" + EnvString("PROJECT_ENV", "development")
+
+	// DB Config
+	dat, err := ioutil.ReadFile(envStr + "/db.json")
 	if err != nil {
 		log.WithFields(
 			log.Fields{
@@ -43,12 +50,34 @@ func LoadConfig() DbConfig {
 		).Fatal("Load Error")
 	}
 
+	// AWS Config
+	dat, err = ioutil.ReadFile(envStr + "/aws.json")
+	if err != nil {
+		log.WithFields(
+			log.Fields{
+				"prefix": "Config",
+				"error":  err.Error(),
+			},
+		).Fatal("Read Error")
+	}
+
+	var aws AwsConfig
+	err = json.Unmarshal(dat, &aws)
+	if err != nil {
+		log.WithFields(
+			log.Fields{
+				"prefix": "Config",
+				"error":  err.Error(),
+			},
+		).Fatal("Load Error")
+	}
+
 	log.WithFields(log.Fields{
 		"prefix": "Config",
 		"env":    EnvString("PROJECT_ENV", "development"),
 	}).Info("Init Done")
 
-	return cfg
+	return cfg, aws
 }
 
 func EnvString(env, fallback string) string {
