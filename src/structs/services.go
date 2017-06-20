aa1030f486ae9d6b677a3f0bdcb01f0fa61dfc1a
Caravus <caravus@caravus.org>
2017-06-20 11:40:44 +0300

    new mobilink fields

--- src/structs/services.go
+++ src/structs/services.go
@@ -17,14 +17,16 @@ type Service struct {
 
 type ProviderOpts struct {
 	// Comment format: Providers where it used (separated by comma) - comment
-	SMSOnContent      string `json:"sms_on_content,omitempty"`      // Mobilink, QRTech - Текст СМС который отправляется при отправке контента. (FYI: может содержать URL и текст на разных языках)
-	SMSOnSubscribe    string `json:"sms_on_unsubscribe,omitempty"`  // Mobilink - Текст СМС, который отправляется при подписке на сервис.
-	SMSOnUnsubscribe  string `json:"sms_on_subscribe,omitempty"`    // Mobilink - Текст СМС, который отправляется при отписке от сервиса.
-	RetryDays         int    `json:"retry_days,omitempty"`          // Mobilink - days to keep retries, for periodic - subscription is alive
-	InactiveDays      int    `json:"inactive_days,omitempty"`       // Mobilink - days of unsuccessful charge turns subscription into inactive state
-	GraceDays         int    `json:"grace_days,omitempty"`          // Mobilink - days in end of subscription period where always must be charged OK
-	PeriodicDays      string `json:"periodic_days,omitempty"`       // Mobilink - days of week to charge subscriber
-	MinimalTouchTimes int    `json:"minimal_touch_times,omitempty"` // Mobilink - минимальное количество скачиваний контента пользователем в рамках одной подписки, чтобы подписка не была отменена.
+	SMSOnContent      string   `json:"sms_on_content,omitempty"`      // Mobilink, QRTech - Текст СМС который отправляется при отправке контента. (FYI: может содержать URL и текст на разных языках)
+	SMSOnSubscribe    string   `json:"sms_on_unsubscribe,omitempty"`  // Mobilink - Текст СМС, который отправляется при подписке на сервис.
+	SMSOnUnsubscribe  string   `json:"sms_on_subscribe,omitempty"`    // Mobilink - Текст СМС, который отправляется при отписке от сервиса.
+	RetryDays         int      `json:"retry_days,omitempty"`          // Mobilink - days to keep retries, for periodic - subscription is alive
+	InactiveDays      int      `json:"inactive_days,omitempty"`       // Mobilink - days of unsuccessful charge turns subscription into inactive state
+	GraceDays         int      `json:"grace_days,omitempty"`          // Mobilink - days in end of subscription period where always must be charged OK
+	PeriodicDays      []string `json:"periodic_days,omitempty"`       // Mobilink - days of week to charge subscriber
+	MinimalTouchTimes int      `json:"minimal_touch_times,omitempty"` // Mobilink - минимальное количество скачиваний контента пользователем в рамках одной подписки, чтобы подписка не была отменена.
+	TrialDays         int      `json:"trial_days,omitempty"`          // Mobilink - Trial Days
+	PurgeAfterDays    int      `json:"purge_after_days,omitempty"`    // Mobilink - Days before Purge policy
 
 	// Other
 	PaidHours           int    `json:"paid_hours,omitempty"`            // rejected rule
